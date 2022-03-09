3e6dc3065cdc39ab284ecc90c086a8d7425b1351
Pavel Kolomnikov <q11254@gmail.com>
2022-03-09 12:41:00 +0700

    add server logic

---
+++ framework/server.js
@@ -0,0 +1,89 @@
+const net = require('net');
+
+class WSClient {
+  constructor(socket) {
+    this.socket = socket;
+  }
+
+  send(payload) {
+    this.socket.write(JSON.stringify(payload));
+  }
+
+  notify(payload) {
+    this.send({ status: 'ok', payload });
+  }
+
+  onClose(cb) {
+    this.socket.on('close', cb);
+  }
+}
+
+const bufferToJson = (buffer) => JSON.parse(buffer.toString());
+class WSServer {
+  constructor(application, { port }) {
+    this.port = port;
+    this.application = application;
+    this.sockets = new Set();
+  }
+
+  start() {
+    const server = net.createServer();
+
+    server.on('connection', (socket) => {
+      this.sockets.add(socket);
+      this.application.console.log(`New connetcion`);
+      this.application.console.log(`Connection count: ${this.sockets.size}`);
+      socket.on('close', () => {
+        this.sockets.delete(socket);
+        this.application.console.log(`Connetcion close`);
+        this.application.console.log(`Connection count: ${this.sockets.size}`);
+      });
+      const client = new WSClient(socket);
+      socket.on('data', (data) => this.onData(client, data));
+    });
+
+    server.on('error', (err) => {
+      this.application.console.log('Server error', err);
+    });
+
+    this.application.console.log(
+      `Server start on: http://localhost:${this.port}/`
+    );
+    server.listen(this.port);
+  }
+
+  async onData(client, buffer) {
+    const { method, payload } = bufferToJson(buffer);
+    this.application.console.log(`New fetch to: ${method}`);
+    const api = this.application.api.tree[method];
+    this.application.context.client = client;
+    const response = await this.makeResponse(api, method, payload);
+    delete this.application.context.client;
+    client.send(response);
+  }
+
+  async makeResponse(api, method, payload) {
+    // TODO simplify
+    if (!api) {
+      const error = `Unknown method: ${method}`;
+      this.application.console.error(error);
+      return { status: 'error', error };
+    }
+    try {
+      const response = await api.method(payload);
+      return {
+        status: 'ok',
+        payload: response,
+      };
+    } catch (error) {
+      this.application.console.error(`Error in method${method}!`, error);
+      if (!api.reject) {
+        return { status: 'error' };
+      }
+      const errorMsg = api.reject(error, payload);
+      return { status: 'error', error: errorMsg };
+    }
+  }
+}
+
+module.exports = WSServer;
